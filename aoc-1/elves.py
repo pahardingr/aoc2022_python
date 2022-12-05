373f92f472fa1d422768c5727dea709cdced27a0
Phillip <pahardingr@gmail.com>
2022-12-04 21:47:01 -0500

    day 1 jupyter notebook transfer

---
+++ aoc-1/elves.py
@@ -0,0 +1,21 @@
+with open("input.txt") as aocd1:
+    cal_list = aocd1.read().splitlines()
+
+elves=[]
+elf=[]
+
+for k in cal_list:
+    if k.isdigit():
+        elf.append(int(k))
+    else:
+        elves.append(elf.copy())
+        elf.clear()
+
+sum_list = []
+
+for elf in elves:
+    sum_list.append(sum(elf))
+
+max(sum_list)
+
+top_3 = sum(sum_list[0:3])
