3db9fae6531aeb4f65a251715f86eaa9947d46b9
Phillip Harding <pahardingr@gmail.com>
2022-12-05 21:37:55 -0500

    I have a list of each line, now I need

--- aoc-2/rps.py
+++ aoc-2/rps.py
@@ -1,3 +1,14 @@
 with open("input.txt", "r") as aocd2:
-    play_list = aocd2.read()
+    play_list = aocd2.read().splitlines()
+
+print(play_list)
+# opp_play = []
+# my_play = []
+#
+# for p in play_list:
+#     for i in range(len(play_list)):
+#         if p == 2*i-1:
+#             opp_play.append(p)
+#         else:
+#             my_play.append(p)
 
