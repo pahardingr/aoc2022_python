7d618351d9c00c1d2620e6c94dbbf2a642eb66a5
Phillip Harding <pahardingr@gmail.com>
2022-12-05 21:47:57 -0500

    No idea but I need a break

--- aoc-2/rps.py
+++ aoc-2/rps.py
@@ -1,14 +1,15 @@
 with open("input.txt", "r") as aocd2:
     play_list = aocd2.read().split()
 
-print(play_list)
-# opp_play = []
-# my_play = []
-#
-# for p in play_list:
-#     for i in range(len(play_list)):
-#         if p == 2*i-1:
-#             opp_play.append(p)
-#         else:
-#             my_play.append(p)
+opp_play = []
+my_play = []
+
+for p in play_list:
+    for i in range(len(play_list)):
+        if i == 2*i-1:
+            opp_play.append(p)
+        else:
+            my_play.append(p)
+            opp_play.clear()
+print(opp_play)
 
